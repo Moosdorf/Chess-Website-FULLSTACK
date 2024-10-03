6eff8e2142e52b4f04216e6170d40ed4ac525e9d
Moosdorf <90873947+Moosdorf@users.noreply.github.com>
2024-10-03 16:51:41 +0200

    Chess webpage start
    I have made the groundwork for the database DDL, included with an ER diagram. This will be added upon if the need arises.
    
    Missing basic functionality for the database.

---
+++ Chess/public/Chess DDL.sql
@@ -0,0 +1,103 @@
+-- drops relations then entities
+
+-- r
+drop table if exists chess_game_moves;
+drop table if exists player_chessgame;
+drop table if exists play_move;
+drop table if exists player_session;
+
+
+
+-- e
+drop table if exists chess_game;
+drop table if exists chess_moves;
+drop table if exists player;
+drop table if exists sessions;
+drop table if exists customization;
+
+
+
+-- Entities
+
+create table chess_game (
+  chess_id serial primary key,
+  player_id1 int,
+  player_id2 int,
+  win varchar default null,
+  on_going varchar default 'on-going',
+  started timestamp,
+  ended timestamp default null
+);
+
+create table chess_moves (
+  move_id varchar primary key not null,
+  player int not null,
+  move varchar not null,
+  move_number int not null
+);
+
+create table player (
+  p_id serial primary key,
+  username varchar not null,
+  password varchar not null,
+  email varchar not null
+);
+
+create table sessions(
+  session_id serial primary key,
+  created_at timestamp,
+  last_seen timestamp,
+  expiration_token varchar
+);
+
+create table customization(
+  cust_id serial primary key,
+  board_pref varchar,
+  piece_pref varchar,
+  dark_mode bool,
+  sound_volume int
+);
+
+-- relations
+
+create table chess_game_moves (
+  move_id varchar not null,
+  chess_id int not null,
+  primary key(move_id, chess_id),
+  foreign key (move_id) references chess_moves,
+  foreign key (chess_id) references chess_game
+);
+
+create table player_chessgame (
+  p_id int,
+  chess_id int,
+  primary key(p_id, chess_id),
+  foreign key (p_id) references player,
+  foreign key (chess_id) references chess_game
+);
+
+create table play_move (
+  p_id int,
+  move_id varchar,
+  move varchar,
+  primary key(p_id, move_id),
+  foreign key (p_id) references player,
+  foreign key (move_id) references chess_moves
+);
+
+create table player_session (
+  u_id int,
+  session_id int,
+  primary key (u_id, session_id),
+  foreign key (u_id) references player,
+  foreign key (session_id) references sessions
+
+);
+
+create table player_customization (
+  u_id int,
+  cust_id int,
+  primary key (u_id, cust_id),
+  foreign key (u_id) references player,
+  foreign key (cust_id) references customization
+);
