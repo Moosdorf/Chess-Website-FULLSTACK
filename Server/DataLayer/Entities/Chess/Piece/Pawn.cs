abe450516363c20f37beadf48892caef16b9a5e9
Moosdorf <90873947+Moosdorf@users.noreply.github.com>
2025-06-17 02:32:01 +0200

    fixet out of bounds fejl ved pawn moves, mangler stadig en passant og promotion

--- Server/DataLayer/Entities/Chess/Piece/Pawn.cs
+++ Server/DataLayer/Entities/Chess/Piece/Pawn.cs
@@ -16,29 +16,34 @@ namespace DataLayer.Entities.Chess.Piece
             // moving 
             if (this.Moves == 0) // can move two squares
             {
-                for (int i = 1; i<3; i++)
+                for (int i = 1; i < 3; i++)
                 {
-                    piece = board[(IsWhite) ? row + i : row - i][col];
+                    piece = board[(IsWhite) ? row + i : row - i][col]; // check two in front
                     if (piece.Type == "empty") AvailableMoves.Add(piece.Position);
+                    else break;
                 }
             } else
             {
-                piece = board[(IsWhite) ? row + 1 : row - 1][col];
-                if (piece.Type == "empty") AvailableMoves.Add(piece.Position);
+                if (row + 1 < 8 && row - 1 >= 0)
+                {
+                    piece = board[(IsWhite) ? row + 1 : row - 1][col]; // check only one in front
+                    if (piece.Type == "empty") AvailableMoves.Add(piece.Position);
+                }
+
             }
 
             // captures left
-            if (col - 1 >= 0)
+            if (col - 1 >= 0 && row + 1 < 8 && row - 1 >= 0)
             {
-                piece = board[(IsWhite) ? row + 1 : row - 1][col - 1];
-                if (piece.Type != "empty") AvailableMoves.Add(piece.Position);
+                piece = board[(IsWhite) ? row + 1 : row - 1][col - 1]; // check left-side capture, if the piece is not at the edge
+                if (piece.Type != "empty" && piece.IsWhite != IsWhite) AvailableMoves.Add(piece.Position);
             }
 
             // captures right
-            if (col + 1 <= 7)
+            if (col + 1 <= 7 && row + 1 < 8 && row - 1 >= 0)
             {
-                piece = board[(IsWhite) ? row + 1 : row - 1][col + 1];
-                if (piece.Type != "empty") AvailableMoves.Add(piece.Position);
+                piece = board[(IsWhite) ? row + 1 : row - 1][col + 1]; // same but for right-side
+                if (piece.Type != "empty" && piece.IsWhite != IsWhite) AvailableMoves.Add(piece.Position);
             }
         }
 
